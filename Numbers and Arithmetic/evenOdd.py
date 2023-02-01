4cec6f00061efe8bee00bd78ddbf8db76518a416
Abhishek V Tatachar <abhishektatachar@gmail.com>
2023-02-01 22:53:27 +0530

    EDIT - Added the problem statement for evenOdd.py

--- Numbers and Arithmetic/evenOdd.py
+++ Numbers and Arithmetic/evenOdd.py
@@ -1,3 +1,7 @@
+"""
+Write a program that takes an integer input from the user and determines if it is even or not. 
+The program should print 'Even' if the number is even, and 'Odd' if the number is odd
+"""
 def determineEvenOdd(number):
     if number%2 == 0:
         print(str(number) + " is even")
